8ffacabb37131a289efb122f965bf9e21c2bc47b
Daniel Zotin <danielzotin@gmail.com>
2020-10-19 22:00:00 -0700

    Update

--- rogudators_comic_panel_generator/__init__.py
+++ rogudators_comic_panel_generator/__init__.py
@@ -1,10 +1,10 @@
 from krita import DockWidgetFactory, DockWidgetFactoryBase
-from .rogudators_comic_panel_generator import RCPG_Docker
+from .rogudators_comic_panel_generator import RCPGWindow
 
-DOCKER_ID = 'comic_panel_generator'
+DOCKER_ID = 'rogudators_comic_panel_generator'
 instance = Krita.instance()
 dock_widget_factory = DockWidgetFactory(DOCKER_ID,
                                         DockWidgetFactoryBase.DockRight,
-                                        RCPG_Docker)
+                                        RCPGWindow)
 
 instance.addDockWidgetFactory(dock_widget_factory)
